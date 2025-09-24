b276fa36a11b07f1d8dd613f0c17324211cfe051
Karthikeyakunda <karthikeya.kunda25@gmail.com>
2025-09-24 12:16:01 +0530

    first commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            Console.WriteLine("world");
+        }
+    }
+}
