4150f75ee50019c64bad22e4f8b2881a28f27c8c
Karthikeya Kunda <159269609+Karthikeyakunda@users.noreply.github.com>
2025-09-24 12:31:38 +0530

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,7 +8,8 @@ namespace ConsoleApp2
         {
             int a = 10;
             int b = 20;
-            Console.WriteLine($"Addition of two numbers:{a} + {b} = {a+b}");   
+            Console.WriteLine($"Addition of two numbers:{a} + {b} = {a+b}"); 
+            Console.WriteLine($"Multiplication of two numbers:{a} x {b} = {a*b}");
         }
     }
 }
